1b194f4d792dc45cb0b8bdf34666aced7ee3699a
dile <dile2011@163.com>
2018-05-25 18:18:52 +0800

    update code

--- core/src/main/java/org/pentaho/platform/engine/core/system/objfac/spring/BeanPublishParser.java
+++ core/src/main/java/org/pentaho/platform/engine/core/system/objfac/spring/BeanPublishParser.java
@@ -34,155 +34,158 @@ import java.util.List;
 import java.util.UUID;
 
 /**
- * Parses the publish tag of a bean. Exposing the bean to the PentahoSystem as an implementation of the given type.
- * Beans can be published by a given type, as all implemented interfaces, as all inherited classes, a combination of all
+ * Parses the publish tag of a bean. Exposing the bean to the PentahoSystem as
+ * an implementation of the given type. Beans can be published by a given type,
+ * as all implemented interfaces, as all inherited classes, a combination of all
  * classes and interfaces, or by default as the class of the bean itself.
  * <p/>
- * Attributes embedded in the publish tag become available to the PentahoSystem to allow for querying of registered
- * implementations. An attribute of "priority" is used to determine the order of registered implementations.
+ * Attributes embedded in the publish tag become available to the PentahoSystem
+ * to allow for querying of registered implementations. An attribute of
+ * "priority" is used to determine the order of registered implementations.
  * <p/>
- * <pen:bean class="com.foo.Clazz"> <pen:publish as-type="[ INTERFACES | CLASSES | ALL | Classname ]> <pen:attributes>
- * <pen:attr key="priority" value="50"/> </pen:attributes> </pen:publish> </pen:bean>
+ * <pen:bean class="com.foo.Clazz"> <pen:publish as-type="[ INTERFACES | CLASSES
+ * | ALL | Classname ]> <pen:attributes> <pen:attr key="priority" value="50"/>
+ * </pen:attributes> </pen:publish> </pen:bean>
  * <p/>
  * User: nbaker Date: 3/27/13
  */
 public class BeanPublishParser implements BeanDefinitionDecorator {
-  private static String ATTR = "as-type";
-  private IPluginManager pluginManager;
+		private static String ATTR = "as-type";
+		private IPluginManager pluginManager;
 
-  private static enum specialPublishTypes {
-    INTERFACES, CLASSES, ALL
-  }
+		private static enum specialPublishTypes {
+				INTERFACES, CLASSES, ALL
+		}
 
-  @Override
-  public BeanDefinitionHolder decorate( Node node, BeanDefinitionHolder beanDefinitionHolder,
-                                        ParserContext parserContext ) {
+		@Override
+		public BeanDefinitionHolder decorate(Node node, BeanDefinitionHolder beanDefinitionHolder, ParserContext parserContext) {
 
-    String publishType = null;
-    String beanClassName;
+				String publishType = null;
+				String beanClassName;
 
-    // If this is a republish of a pen:bean, the class will be a FactoryBean and the actual class returned back the
-    // factory will be stored as an attribute.
-    if ( beanDefinitionHolder.getBeanDefinition().getAttribute( "originalClassName" ) != null ) {
-      beanClassName = beanDefinitionHolder.getBeanDefinition().getAttribute( "originalClassName" ).toString();
-    } else {
-      beanClassName = beanDefinitionHolder.getBeanDefinition().getBeanClassName();
-    }
+				// If this is a republish of a pen:bean, the class will be a FactoryBean and the
+				// actual class returned back the
+				// factory will be stored as an attribute.
+				if (beanDefinitionHolder.getBeanDefinition().getAttribute("originalClassName") != null) {
+						beanClassName = beanDefinitionHolder.getBeanDefinition().getAttribute("originalClassName").toString();
 
-    if ( node.getAttributes().getNamedItem( ATTR ) != null ) { // publish as type if specified
-      publishType = node.getAttributes().getNamedItem( ATTR ).getNodeValue();
-    } else { // fallback to publish as itself
-      publishType = beanClassName;
-    }
+				} else {
+						beanClassName = beanDefinitionHolder.getBeanDefinition().getBeanClassName();
+				}
 
-    // capture the "ID" of the bean as an attribute so it can be queried for
-    beanDefinitionHolder.getBeanDefinition().setAttribute( "id", beanDefinitionHolder.getBeanName() );
-    NodeList nodes = node.getChildNodes();
+				if (node.getAttributes().getNamedItem(ATTR) != null) { // publish as type if specified
+						publishType = node.getAttributes().getNamedItem(ATTR).getNodeValue();
 
-    for ( int i = 0; i < nodes.getLength(); i++ ) {
-      Node n = nodes.item( i );
-      if ( stripNamespace( n.getNodeName() ).equals( Const.ATTRIBUTES ) ) {
-        NodeList attrnodes = n.getChildNodes();
+				} else { // fallback to publish as itself
+						publishType = beanClassName;
+				}
 
-        for ( int y = 0; y < attrnodes.getLength(); y++ ) {
-          Node an = attrnodes.item( y );
-          if ( stripNamespace( an.getNodeName() ).equals( Const.ATTR ) ) {
-            beanDefinitionHolder.getBeanDefinition().setAttribute(
-              an.getAttributes().getNamedItem( Const.KEY ).getNodeValue(),
-              an.getAttributes().getNamedItem( Const.VALUE ).getNodeValue() );
-          }
-        }
-      }
-    }
+				// capture the "ID" of the bean as an attribute so it can be queried for
+				beanDefinitionHolder.getBeanDefinition().setAttribute("id", beanDefinitionHolder.getBeanName());
+				NodeList nodes = node.getChildNodes();
 
-    try {
-      List<Class<?>> classesToPublish = new ArrayList<Class<?>>();
+				for (int i = 0; i < nodes.getLength(); i++) {
+						Node n = nodes.item(i);
+						if (stripNamespace(n.getNodeName()).equals(Const.ATTRIBUTES)) {
+								NodeList attrnodes = n.getChildNodes();
 
-      Class<?> clazz = findClass( beanClassName );
+								for (int y = 0; y < attrnodes.getLength(); y++) {
+										Node an = attrnodes.item(y);
+										if (stripNamespace(an.getNodeName()).equals(Const.ATTR)) {
+												beanDefinitionHolder.getBeanDefinition().setAttribute(an.getAttributes().getNamedItem(Const.KEY).getNodeValue(),
+												                an.getAttributes().getNamedItem(Const.VALUE).getNodeValue());
+										}
+								}
+						}
+				}
 
-      if ( specialPublishTypes.INTERFACES.name().equals( publishType ) ) {
-        if ( clazz.isInterface() ) { // publish as self if interface already (re-publish flow)
-          classesToPublish.add( clazz );
-        } else {
-          classesToPublish.addAll( ClassUtils.getAllInterfaces( clazz ) );
-        }
-      } else if ( specialPublishTypes.CLASSES.name().equals( publishType ) ) {
+				try {
+						List<Class<?>> classesToPublish = new ArrayList<Class<?>>();
 
-        classesToPublish.addAll( ClassUtils.getAllSuperclasses( clazz ) );
-        classesToPublish.add( clazz );
-      } else if ( specialPublishTypes.ALL.name().equals( publishType ) ) {
+						Class<?> clazz = findClass(beanClassName);
 
-        classesToPublish.addAll( ClassUtils.getAllInterfaces( clazz ) );
-        classesToPublish.addAll( ClassUtils.getAllSuperclasses( clazz ) );
-        classesToPublish.add( clazz );
-      } else {
-        classesToPublish.add( getClass().getClassLoader().loadClass( publishType ) );
-      }
+						if (specialPublishTypes.INTERFACES.name().equals(publishType)) {
+								if (clazz.isInterface()) { // publish as self if interface already (re-publish flow)
+										classesToPublish.add(clazz);
 
-      String beanFactoryId = null;
+								} else {
+										classesToPublish.addAll(ClassUtils.getAllInterfaces(clazz));
+								}
 
-      if ( parserContext.getRegistry().containsBeanDefinition( Const.FACTORY_MARKER ) == false ) {
-        beanFactoryId = UUID.randomUUID().toString();
-        parserContext.getRegistry().registerBeanDefinition(
-          Const.FACTORY_MARKER,
-          BeanDefinitionBuilder.genericBeanDefinition( Marker.class ).setScope( BeanDefinition.SCOPE_PROTOTYPE )
-            .addConstructorArgValue( beanFactoryId ).getBeanDefinition() );
-      } else {
-        beanFactoryId =
-          (String) parserContext.getRegistry().getBeanDefinition( Const.FACTORY_MARKER )
-            .getConstructorArgumentValues().getArgumentValue( 0, String.class ).getValue();
-      }
+						} else if (specialPublishTypes.CLASSES.name().equals(publishType)) {
+								classesToPublish.addAll(ClassUtils.getAllSuperclasses(clazz));
+								classesToPublish.add(clazz);
 
-      for ( Class cls : classesToPublish ) {
-        PublishedBeanRegistry.registerBean( beanDefinitionHolder.getBeanName(), cls, beanFactoryId );
-      }
+						} else if (specialPublishTypes.ALL.name().equals(publishType)) {
 
-    } catch ( ClassNotFoundException e ) {
-      throw new RuntimeException( "Cannot find class for publish type: " + publishType
-        + " specified on publish of bean id: " + beanDefinitionHolder.getBeanName(), e );
-    }
-    return beanDefinitionHolder;
-  }
+								classesToPublish.addAll(ClassUtils.getAllInterfaces(clazz));
+								classesToPublish.addAll(ClassUtils.getAllSuperclasses(clazz));
+								classesToPublish.add(clazz);
+						} else {
+								classesToPublish.add(getClass().getClassLoader().loadClass(publishType));
+						}
 
-  private Class<?> findClass( String beanClassName ) throws ClassNotFoundException {
-    // try main classloader
-    // getClass().getClassLoader()
-    Class<?> clazz = loadClassFromClassloader( getClass().getClassLoader(), beanClassName );
-    if ( clazz != null ) {
-      return clazz;
-    }
-    if (getPluginManager() != null) {
-      for ( String s : getPluginManager().getRegisteredPlugins() ) {
-        clazz = loadClassFromClassloader( getPluginManager().getClassLoader( s ), beanClassName );
-        if ( clazz != null ) {
-          return clazz;
-        }
-      }
-    }
+						String beanFactoryId = null;
 
-    throw new ClassNotFoundException( beanClassName );
-  }
+						if (parserContext.getRegistry().containsBeanDefinition(Const.FACTORY_MARKER) == false) {
+								beanFactoryId = UUID.randomUUID().toString();
+								parserContext.getRegistry().registerBeanDefinition(Const.FACTORY_MARKER, BeanDefinitionBuilder.genericBeanDefinition(Marker.class)
+								                .setScope(BeanDefinition.SCOPE_PROTOTYPE).addConstructorArgValue(beanFactoryId).getBeanDefinition());
+						} else {
+								beanFactoryId = (String) parserContext.getRegistry().getBeanDefinition(Const.FACTORY_MARKER).getConstructorArgumentValues()
+								                .getArgumentValue(0, String.class).getValue();
+						}
 
-  private IPluginManager getPluginManager() {
-    if ( pluginManager == null ) {
-      pluginManager = PentahoSystem.get( IPluginManager.class );
-    }
-    return pluginManager;
-  }
+						for (Class cls : classesToPublish) {
+								PublishedBeanRegistry.registerBean(beanDefinitionHolder.getBeanName(), cls, beanFactoryId);
+						}
 
-  private Class<?> loadClassFromClassloader( ClassLoader loader, String beanClassName ) {
-    try {
-      return loader.loadClass( beanClassName );
-    } catch ( ClassNotFoundException e ) {
-      //ignored
-    }
-    return null;
-  }
+				} catch (ClassNotFoundException e) {
+						throw new RuntimeException("Cannot find class for publish type: " + publishType + " specified on publish of bean id: " + beanDefinitionHolder.getBeanName(),
+						                e);
+				}
+				return beanDefinitionHolder;
+		}
 
-  private static String stripNamespace( String s ) {
-    if ( s.indexOf( ':' ) > 0 ) {
-      return s.substring( s.indexOf( ':' ) + 1 );
-    }
-    return s;
-  }
+		private Class<?> findClass(String beanClassName) throws ClassNotFoundException {
+				// try main classloader
+				// getClass().getClassLoader()
+				Class<?> clazz = loadClassFromClassloader(getClass().getClassLoader(), beanClassName);
+				if (clazz != null) {
+						return clazz;
+				}
+				if (getPluginManager() != null) {
+						for (String s : getPluginManager().getRegisteredPlugins()) {
+								clazz = loadClassFromClassloader(getPluginManager().getClassLoader(s), beanClassName);
+								if (clazz != null) {
+										return clazz;
+								}
+						}
+				}
+
+				throw new ClassNotFoundException(beanClassName);
+		}
+
+		private IPluginManager getPluginManager() {
+				if (pluginManager == null) {
+						pluginManager = PentahoSystem.get(IPluginManager.class);
+				}
+				return pluginManager;
+		}
+
+		private Class<?> loadClassFromClassloader(ClassLoader loader, String beanClassName) {
+				try {
+						return loader.loadClass(beanClassName);
+				} catch (ClassNotFoundException e) {
+						// ignored
+				}
+				return null;
+		}
+
+		private static String stripNamespace(String s) {
+				if (s.indexOf(':') > 0) {
+						return s.substring(s.indexOf(':') + 1);
+				}
+				return s;
+		}
 }
--- core/src/main/java/org/pentaho/platform/engine/services/connection/datasource/dbcp/BaseDatasourceService.java
+++ core/src/main/java/org/pentaho/platform/engine/services/connection/datasource/dbcp/BaseDatasourceService.java
@@ -31,183 +31,195 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public abstract class BaseDatasourceService implements IDBDatasourceService {
-  ICacheManager cacheManager;
+		ICacheManager cacheManager;
 
-  public BaseDatasourceService() {
-    cacheManager = getCacheManager();
-    // if no cache manager implementation is available we'll use the simple one
-  }
+		public BaseDatasourceService() {
+				cacheManager = getCacheManager();
+				// if no cache manager implementation is available we'll use the simple one
+		}
 
-  /**
-   * This method clears the JNDI DS cache. The need exists because after a JNDI connection edit the old DS must be
-   * removed from the cache.
-   * 
-   */
-  public void clearCache() {
-    cacheManager.removeRegionCache( IDBDatasourceService.JDBC_DATASOURCE );
-  }
+		/**
+		 * This method clears the JNDI DS cache. The need exists because after a JNDI
+		 * connection edit the old DS must be removed from the cache.
+		 * 
+		 */
+		public void clearCache() {
+				cacheManager.removeRegionCache(IDBDatasourceService.JDBC_DATASOURCE);
+		}
 
-  /**
-   * This method clears the JNDI DS cache. The need exists because after a JNDI connection edit the old DS must be
-   * removed from the cache.
-   * 
-   */
-  public void clearDataSource( String dsName ) {
-    cacheManager.removeFromRegionCache( IDBDatasourceService.JDBC_DATASOURCE, dsName );
-  }
+		/**
+		 * This method clears the JNDI DS cache. The need exists because after a JNDI
+		 * connection edit the old DS must be removed from the cache.
+		 * 
+		 */
+		public void clearDataSource(String dsName) {
+				cacheManager.removeFromRegionCache(IDBDatasourceService.JDBC_DATASOURCE, dsName);
+		}
 
-  public DataSource getDataSource( String dsName ) throws DBDatasourceServiceException {
-    DataSource dataSource = null;
-    if ( cacheManager != null ) {
-      if ( !cacheManager.cacheEnabled( IDBDatasourceService.JDBC_DATASOURCE ) ) {
-        cacheManager.addCacheRegion( IDBDatasourceService.JDBC_DATASOURCE );
-      }
-      Object foundDs = cacheManager.getFromRegionCache( IDBDatasourceService.JDBC_DATASOURCE, dsName );
-      if ( foundDs != null ) {
-        dataSource = (DataSource) foundDs;
-      } else {
-        dataSource = retrieve( dsName );
-      }
-    }
-    return dataSource;
-  }
+		public DataSource getDataSource(String dsName) throws DBDatasourceServiceException {
+				DataSource dataSource = null;
+				if (cacheManager != null) {
+						if (!cacheManager.cacheEnabled(IDBDatasourceService.JDBC_DATASOURCE)) {
+								cacheManager.addCacheRegion(IDBDatasourceService.JDBC_DATASOURCE);
+						}
+						Object foundDs = cacheManager.getFromRegionCache(IDBDatasourceService.JDBC_DATASOURCE, dsName);
+						if (foundDs != null) {
+								dataSource = (DataSource) foundDs;
+						} else {
+								dataSource = retrieve(dsName);
+						}
+				}
+				return dataSource;
+		}
 
-  /**
-   * This should have been abstract, but changes to this API at a point release is not advised.
-   * @param name name of JNDI reference
-   * @return DataSource
-   * @throws DBDatasourceServiceException
-   */
-  protected DataSource retrieve( String name ) throws DBDatasourceServiceException {
-    return null;
-  }
+		/**
+		 * This should have been abstract, but changes to this API at a point release is
+		 * not advised.
+		 * 
+		 * @param name
+		 *                name of JNDI reference
+		 * @return DataSource
+		 * @throws DBDatasourceServiceException
+		 */
+		protected DataSource retrieve(String name) throws DBDatasourceServiceException {
+				return null;
+		}
 
-  /**
-   * This should have been abstract, but changes to this API at a point release is not advised.
-   *
-   * @param databaseConnection
-   * @return
-   * @throws DBDatasourceServiceException
-   */
-  protected DataSource resolveDatabaseConnection( IDatabaseConnection databaseConnection )
-      throws DBDatasourceServiceException {
-    return null;
-  }
+		/**
+		 * This should have been abstract, but changes to this API at a point release is
+		 * not advised.
+		 *
+		 * @param databaseConnection
+		 * @return
+		 * @throws DBDatasourceServiceException
+		 */
+		protected DataSource resolveDatabaseConnection(IDatabaseConnection databaseConnection) throws DBDatasourceServiceException {
+				return null;
+		}
 
+		protected DataSource getJndiDataSource(final String dsName) throws DBDatasourceServiceException {
+				return PooledDatasourceHelper.getJndiDataSource(dsName);
+		}
 
-  protected DataSource getJndiDataSource( final String dsName ) throws DBDatasourceServiceException {
-    return PooledDatasourceHelper.getJndiDataSource( dsName );
-  }
+		/**
+		 * Since JNDI is supported different ways in different app servers, it's nearly
+		 * impossible to have a ubiquitous way to look up a datasource. This method is
+		 * intended to hide all the lookups that may be required to find a jndi name,
+		 * and return the actual bound name.
+		 * 
+		 * @param dsName
+		 *                The Datasource name (like SampleData)
+		 * @return The bound DS name if it is bound in JNDI (like "jdbc/SampleData")
+		 * @throws DBDatasourceServiceException
+		 */
+		public String getDSBoundName(final String dsName) throws DBDatasourceServiceException {
+				try {
+						InitialContext ctx = new InitialContext();
+						Object lkup = null;
+						NamingException firstNe = null;
+						String rtn = dsName;
+						
+						// First, try what they ask for...
+						try {
+								lkup = ctx.lookup(rtn);
+								if (lkup != null) {
+										return rtn;
+								}
+								
+						} catch (NamingException ignored) {
+								firstNe = ignored;
+						}
+						
+						try {
+								// Needed this for Jboss
+								rtn = "java:" + dsName; //$NON-NLS-1$
+								lkup = ctx.lookup(rtn);
+								if (lkup != null) {
+										return rtn;
+								}
+								
+						} catch (NamingException ignored) {
+								// ignored
+						}
+						
+						try {
+								// Tomcat
+								rtn = "java:comp/env/jdbc/" + dsName; //$NON-NLS-1$
+								lkup = ctx.lookup(rtn);
+								if (lkup != null) {
+										return rtn;
+								}
+								
+						} catch (NamingException ignored) {
+								// ignored
+						}
+						
+						try {
+								// Others?
+								rtn = "jdbc/" + dsName; //$NON-NLS-1$
+								lkup = ctx.lookup(rtn);
+								if (lkup != null) {
+										return rtn;
+								}
+						} catch (NamingException ignored) {
+								// ignored
+						}
+						
+						if (firstNe != null) {
+								throw new DBDatasourceServiceException(firstNe);
+						}
+						
+						throw new DBDatasourceServiceException(dsName);
+						
+				} catch (NamingException ne) {
+						throw new DBDatasourceServiceException(ne);
+				}
+		}
 
-  /**
-   * Since JNDI is supported different ways in different app servers, it's nearly impossible to have a ubiquitous
-   * way to look up a datasource. This method is intended to hide all the lookups that may be required to find a
-   * jndi name, and return the actual bound name.
-   * 
-   * @param dsName
-   *          The Datasource name (like SampleData)
-   * @return The bound DS name if it is bound in JNDI (like "jdbc/SampleData")
-   * @throws DBDatasourceServiceException
-   */
-  public String getDSBoundName( final String dsName ) throws DBDatasourceServiceException {
-    try {
-      InitialContext ctx = new InitialContext();
-      Object lkup = null;
-      NamingException firstNe = null;
-      String rtn = dsName;
-      // First, try what they ask for...
-      try {
-        lkup = ctx.lookup( rtn );
-        if ( lkup != null ) {
-          return rtn;
-        }
-      } catch ( NamingException ignored ) {
-        firstNe = ignored;
-      }
-      try {
-        // Needed this for Jboss
-        rtn = "java:" + dsName; //$NON-NLS-1$
-        lkup = ctx.lookup( rtn );
-        if ( lkup != null ) {
-          return rtn;
-        }
-      } catch ( NamingException ignored ) {
-        //ignored
-      }
-      try {
-        // Tomcat
-        rtn = "java:comp/env/jdbc/" + dsName; //$NON-NLS-1$
-        lkup = ctx.lookup( rtn );
-        if ( lkup != null ) {
-          return rtn;
-        }
-      } catch ( NamingException ignored ) {
-        //ignored
-      }
-      try {
-        // Others?
-        rtn = "jdbc/" + dsName; //$NON-NLS-1$
-        lkup = ctx.lookup( rtn );
-        if ( lkup != null ) {
-          return rtn;
-        }
-      } catch ( NamingException ignored ) {
-        //ignored
-      }
-      if ( firstNe != null ) {
-        throw new DBDatasourceServiceException( firstNe );
-      }
-      throw new DBDatasourceServiceException( dsName );
-    } catch ( NamingException ne ) {
-      throw new DBDatasourceServiceException( ne );
-    }
-  }
+		/**
+		 * Since JNDI is supported different ways in different app servers, it's nearly
+		 * impossible to have a ubiquitous way to look up a datasource. This method is
+		 * intended to extract just the regular name of a specified JNDI source.
+		 * 
+		 * @param dsName
+		 *                The Datasource name (like "jdbc/SampleData")
+		 * @return The unbound DS name (like "SampleData")
+		 */
+		public String getDSUnboundName(final String dsName) {
+				if (null == dsName) {
+						return null;
+				}
+				final String PREFIX_TOMCAT = "java:comp/env/jdbc/"; //$NON-NLS-1$
+				final String PREFIX_JBOSS = "java:"; //$NON-NLS-1$
+				final String PREFIX_OTHER = "jdbc/"; //$NON-NLS-1$
 
-  /**
-   * Since JNDI is supported different ways in different app servers, it's nearly impossible to have a ubiquitous
-   * way to look up a datasource. This method is intended to extract just the regular name of a specified JNDI
-   * source.
-   * 
-   * @param dsName
-   *          The Datasource name (like "jdbc/SampleData")
-   * @return The unbound DS name (like "SampleData")
-   */
-  public String getDSUnboundName( final String dsName ) {
-    if ( null == dsName ) {
-      return null;
-    }
-    final String PREFIX_TOMCAT = "java:comp/env/jdbc/"; //$NON-NLS-1$
-    final String PREFIX_JBOSS = "java:"; //$NON-NLS-1$
-    final String PREFIX_OTHER = "jdbc/"; //$NON-NLS-1$
+				// order is important here since jboss is a substring of tomcat
+				if (dsName.startsWith(PREFIX_TOMCAT)) {
+						return dsName.substring(PREFIX_TOMCAT.length());
+				} else if (dsName.startsWith(PREFIX_JBOSS)) {
+						return dsName.substring(PREFIX_JBOSS.length());
+				} else if (dsName.startsWith(PREFIX_OTHER)) {
+						return dsName.substring(PREFIX_OTHER.length());
+				} else {
+						// select that last token from the string
+						int last = dsName.lastIndexOf("/"); //$NON-NLS-1$
+						if (last < dsName.lastIndexOf(":")) { //$NON-NLS-1$
+								last = dsName.lastIndexOf(":"); //$NON-NLS-1$
+						}
+						if (last != -1) {
+								return dsName.substring(last + 1);
+						} else {
+								return dsName;
+						}
+				}
+		}
 
-    // order is important here since jboss is a substring of tomcat
-    if ( dsName.startsWith( PREFIX_TOMCAT ) ) {
-      return dsName.substring( PREFIX_TOMCAT.length() );
-    } else if ( dsName.startsWith( PREFIX_JBOSS ) ) {
-      return dsName.substring( PREFIX_JBOSS.length() );
-    } else if ( dsName.startsWith( PREFIX_OTHER ) ) {
-      return dsName.substring( PREFIX_OTHER.length() );
-    } else {
-      // select that last token from the string
-      int last = dsName.lastIndexOf( "/" ); //$NON-NLS-1$
-      if ( last < dsName.lastIndexOf( ":" ) ) { //$NON-NLS-1$
-        last = dsName.lastIndexOf( ":" ); //$NON-NLS-1$
-      }
-      if ( last != -1 ) {
-        return dsName.substring( last + 1 );
-      } else {
-        return dsName;
-      }
-    }
-  }
+		public ICacheManager getCacheManager() {
+				return PentahoSystem.getCacheManager(null);
+		}
 
-  public ICacheManager getCacheManager( ) {
-    return PentahoSystem.getCacheManager( null );
-  }
-
-
-  public IDatasourceMgmtService getDatasourceMgmtService( ) {
-    return (IDatasourceMgmtService) PentahoSystem.get( IDatasourceMgmtService.class, PentahoSessionHolder.getSession() );
-  }
+		public IDatasourceMgmtService getDatasourceMgmtService() {
+				return (IDatasourceMgmtService) PentahoSystem.get(IDatasourceMgmtService.class, PentahoSessionHolder.getSession());
+		}
 
 }
